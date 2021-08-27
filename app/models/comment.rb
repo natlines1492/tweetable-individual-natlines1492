e53a8b94bea13bd73b6beceae0b32b0f4228d510
natlines1492 <natlines14@gmail.com>
2021-08-27 18:01:04 +0000

    Add validation for user, tweets and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
+  validates :body, presence: true
+  validates :body, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :tweets, dependent: :destroy
+  # has_one_attached :avatar
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
