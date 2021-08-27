faad1fc2008357ab77975e9357185684d54a61ce
natlines1492 <natlines14@gmail.com>
2021-08-27 18:52:41 +0000

    Add active storage to userl model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets, dependent: :destroy
-  # has_one_attached :avatar
+  has_one_attached :avatar
 
   validates :username, :email, presence: true, uniqueness: true
   validates :name, presence: true
